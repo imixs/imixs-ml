48079e9bf2ca8824a2b610e581ba9ef41ec4dc89
Ralph Soika <ralph.soika@imixs.com>
2020-04-23 11:23:28 +0200

    refactoring project structure
    Issue #3

---
+++ imixs-ml-learning/src/test/java/org/imixs/ml/ParserTest.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ *  Imixs Workflow 
+ *  Copyright (C) 2001, 2011 Imixs Software Solutions GmbH,  
+ *  http://www.imixs.com
+ *  
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2 
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful, 
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *  General Public License for more details.
+ *  
+ *  You can receive a copy of the GNU General Public
+ *  License at http://www.gnu.org/licenses/gpl.html
+ *  
+ *  Project: 
+ *  	http://www.imixs.org
+ *  	http://java.net/projects/imixs-workflow
+ *  
+ *  Contributors:  
+ *  	Imixs Software Solutions GmbH - initial API and implementation
+ *  	Ralph Soika - Software Developer
+ *******************************************************************************/
+
+package org.imixs.jwt;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.util.logging.Logger;
+
+import javax.crypto.SecretKey;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * Test JWTParser class. Verify a token
+ * 
+ * @author rsoika
+ * 
+ */
+public class ParserTest {
+
+	private static Logger logger = Logger.getLogger(ParserTest.class.getName());
+
+	
+	/**
+	 * Data based on iwt.io debugger example
+	 */
+	@Before
+	public void setup() {
+	}
+
+	/**
+	 * Test 
+	 */
+	@Test
+	public void testVerify(){
+
+	
+
+	}
+
+}
