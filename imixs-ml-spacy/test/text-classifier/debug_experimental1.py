2c6022656b8fd1ca3f223994d6762c7aeebd9620
Ralph Soika <ralph.soika@imixs.com>
2020-12-24 10:50:11 +0100

    added examples

---
+++ imixs-ml-spacy/test/text-classifier/debug_experimental1.py
@@ -0,0 +1,161 @@
+from pydantic import BaseModel
+import spacy
+import random
+import os
+from spacy.util import minibatch, compounding
+#
+# The goal of this test program is to figure out 
+# how we can provide learning data to an empty 
+# spaCy model
+#
+#
+#
+#
+
+
+class Data(BaseModel):
+    text: str 
+
+# Specify the  categories
+LABEL = ['fussball', 'schach']
+
+
+
+TRAIN_DATA = [
+              
+              ('Er schießt mit dem Ball ein Tor', {'cats': {'fussball': True}}),
+              ('Er scheißt den Ball ins Abseits', {'cats': {'fussball': True}}),
+              ('Er scheißt den Ball ins Seitenaus', {'cats': {'fussball': True}}),
+              ('Er scheißt den Ball neben das Tor', {'cats': {'fussball': True}}),
+              ('Er scheißt den Ball über das Tor', {'cats': {'fussball': True}}),
+              ('Er scheißt den Ball an den Balken', {'cats': {'fussball': True}}),
+              ('Er köpft den Ball ins Tor', {'cats': {'fussball': True}}),
+              
+              ('Er zieht die Figur Läufer', {'cats': {'schach': True}}),
+              ('Er zieht die Figur Turm', {'cats': {'schach': True}}),
+              ('Er zieht mit der Figur Bauer', {'cats': {'schach': True}}),
+              ('Er zieht die Figur König', {'cats': {'schach': True}}),
+              ('Er zieht die Figur Dame', {'cats': {'schach': True}})
+              
+             ]
+
+# The learn method should initialize an empty model and add training data.
+#
+# 
+#
+#
+def train(train_data,iterations,modelPath):
+    
+    # Test if the model exists
+    modelExists=os.path.isdir(modelPath)
+     
+     # 1.) load model or create blank Language class
+    """Load the model, set up the pipeline and train the entity recognizer."""
+    if modelExists:
+        nlp = spacy.load(modelPath)  # load existing spaCy model
+        print("Loaded model '%s'" % modelPath)
+    else:
+        # 1.) create blank Language class
+        nlp = spacy.blank("en")  # create blank Language class
+        print("Created blank 'en' model")
+    
+
+    
+    
+    # Initialize a textcat pipe in a spacy pipeline object (nlp), and add the label variable in it.
+
+
+    if 'textcat' not in nlp.pipe_names:
+      print("create pipe 'textcat'...")
+      textcat = nlp.create_pipe("textcat", config={"exclusive_classes": True})
+      nlp.add_pipe(textcat, last=True) 
+    else:
+      textcat = nlp.get_pipe("textcat")
+
+
+    for i in LABEL:
+        print("...adding new label '" + i + "'...")
+        textcat.add_label(i)   # Add new  labels to the categorizer
+
+    
+
+
+    # Iterate the training examples to optimize the model
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'textcat']
+
+    # Only train the textcat pipe
+    with nlp.disable_pipes(*other_pipes):
+        
+                # reset and initialize the weights randomly – but only if we're
+        # training a new model
+        if not modelExists:
+            print("begin new training!")
+            optimizer=nlp.begin_training()
+        
+        
+        
+        #optimizer = nlp.begin_training()
+        print("Training model...")
+        for i in range(iterations):
+            losses = {}
+            batches = minibatch(train_data, size=compounding(4.0, 32.0, 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(texts, annotations,
+                          #sgd=optimizer,
+                          #drop=0.2, 
+                          losses=losses)
+
+
+
+
+        
+      
+
+
+
+
+
+
+
+
+
+
+            print(losses)
+    print("Training completed")
+    return nlp
+
+
+
+# Startup method
+if __name__ == "__main__":
+    #lerne(TRAIN_DATA)
+    prdnlp = train(TRAIN_DATA, 20,"training_model")
+    # save the model...
+    prdnlp.to_disk("training_model")
+    
+    #Test your text
+    #test_text = input("Enter your testing text: ")
+    print("=== testing Fussball...")
+    test_text = "Er schießt mit dem Ball ein Tor"
+    doc = prdnlp(test_text)
+    
+    for label, score in doc.cats.items():
+        print(label, score)
+           
+           
+  
+    #Test your text
+    #test_text = input("Enter your testing text: ")
+    print("=== testing Schach...")
+    test_text = "Er zieht die Figur Dame"
+    doc = prdnlp(test_text)
+    
+    for label, score in doc.cats.items():
+        print(label, score)
+           
+    
+    
+    print("finished")
+    
+    
--- imixs-ml-spacy/test/text-classifier/example_testmodel_exclusive_classes.py
+++ imixs-ml-spacy/test/text-classifier/example_testmodel_exclusive_classes.py
@@ -22,18 +22,26 @@ LABEL = ['invoice', 'offer', 'order']
 
 # Test data with True cats only
 
-TRAIN_DATA = [('Please see our invoice below.', {'cats': {'invoice': True}}),
-              ('Please pay the total amount of our invoice.', {'cats': {'invoice': True}}),
-              ('We send you an invoice regarding your 47445', {'cats': {'invoice': True}}),
+
+# In this example we provide only one category per test row.
+# This needs longer trainging as if the false categories are provided directly per each row
+
+#set exclusive_classes to False as we provide only on category per test row.
+# See: https://spacy.io/api/textcategorizer#init
+
+TRAIN_DATA = [('Please see our invoice below. Total 300,00 EUR IBAN 3355BBBAAAXXX  invoice date 2020-03-03', {'cats': {'invoice': True}}),
+              ('Please see our invoice below. Total 40,00 EUR IBAN 3355BB444AXXX  invoice date 2021-03-03', {'cats': {'invoice': True}}),
+              #('We send you an invoice regarding your 47445', {'cats': {'invoice': True}}),
               
-              ('Please send us an offer.', {'cats': {'offer': True}}),
-              ('Please tell us the price for the parts.', {'cats': {'offer': True}}),
-              ('Please tell us the price.', {'cats': {'offer': True}}),
+              ('Please send us an offer. 300 plates, 20 tables, 100 chairs  date 2020-05-01', {'cats': {'offer': True}}),
+              ('Please send us an offer. 300 tables, 20 chairs  date 2021-05-01', {'cats': {'offer': True}}),
+              #('Please tell us the price for the parts.', {'cats': {'offer': True}}),
+              #('Please tell us the price.', {'cats': {'offer': True}}),
               
-              ('As discussed we would like to order the parts. ', {'cats': {'order': True}}),
-              ('We order the parts. ', {'cats': {'order': True}}),
-              ('Please send us the parts. ', {'cats': {'order': True}}),
-              ('We order the following parts. ', {'cats': {'order': True}})
+              #('As discussed we would like to order the parts. ', {'cats': {'order': True}}),
+              #('We order the parts. ', {'cats': {'order': True}}),
+              #('Please send us the parts. ', {'cats': {'order': True}}),
+              ('We order the following parts. order-no 1145 delivery date 2020-05-01', {'cats': {'order': True}})
              ]
  
 
@@ -67,10 +75,19 @@ def train(train_data,iterations,modelPath):
 
     if 'textcat' not in nlp.pipe_names:
       print("create pipe 'textcat'...")
-      textcat = nlp.create_pipe("textcat")
-      nlp.add_pipe(textcat, last=True) 
+      
+      # create a new pipe with exclusive_classes=true
+      
+      #textcat = nlp.create_pipe("textcat")
+      textcat = nlp.create_pipe("textcat", config={"exclusive_classes": True})
+      nlp.add_pipe(textcat, last=True)
+      #nlp.add_pipe(textcat) 
     else:
       textcat = nlp.get_pipe("textcat")
+
+
+
+
 
 
     for i in LABEL:
@@ -86,18 +103,39 @@ def train(train_data,iterations,modelPath):
     # Only train the textcat pipe
     with nlp.disable_pipes(*other_pipes):
         optimizer = nlp.begin_training()
+        
+        #optimizer = nlp.begin_training(component_cfg={'textcat': {"exclusive_classes": False} })
+
+        
         print("Training model...")
         for i in range(iterations):
+            random.shuffle(train_data)
             losses = {}
             batches = minibatch(train_data, size=compounding(4.0, 32.0, 1.001))
             for batch in batches:
                 texts, annotations = zip(*batch)
-                nlp.update(texts, annotations, sgd=optimizer,
-                          drop=0.2, losses=losses)
+                nlp.update(texts, 
+                           annotations, 
+                           sgd=optimizer,
+                           drop=0.2, 
+                           losses=losses)
 
-            print(losses)
+            print("Losses", losses)
     print("Training completed")
     return nlp
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Startup method
@@ -109,8 +147,8 @@ if __name__ == "__main__":
     
     #Test your text
     #test_text = input("Enter your testing text: ")
-    print("test invoice...")
-    test_text = "Please see our new invoice below."
+    print("=== testing invoice...")
+    test_text = "Please see our invoice below. Total 300,00 EUR IBAN 3355BBBAAAXXX  invoice date 2020-03-03"
     doc = prdnlp(test_text)
     
     for label, score in doc.cats.items():
@@ -120,8 +158,8 @@ if __name__ == "__main__":
   
     #Test your text
     #test_text = input("Enter your testing text: ")
-    print("test order...")
-    test_text = "Please send us the parts."
+    print("=== testing order...")
+    test_text = "We order the following parts. order-no 1145 delivery date 2020-05-01"
     doc = prdnlp(test_text)
     
     for label, score in doc.cats.items():
